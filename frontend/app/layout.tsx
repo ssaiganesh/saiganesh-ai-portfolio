c8d96a4688be4b186e135ce1203f9101c7d222dd
Shankar Sai Ganesh <shankarsaiganesh@gmail.com>
2026-05-30 17:30:34 +0800

    update css path for ci cd failure

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import Link from 'next/link';
-import './globals.css';
+import '../app/globals.css';
 import { AIChatbot } from '../components/AIChatbot';
 
 export const metadata: Metadata = {
